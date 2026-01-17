d46868ed1a01633be9a50b1d70483a5f8af57f6f
Sanjeev Midda <s.midda1@gmail.com>
2026-01-17 21:17:25 +0000

    setting up useState with our transaction data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import "./index.css";
+import { useState } from "react";
 
 function App() {
+  const [transactions, setTransactions] = useState([
+    { id: 1, type: "income", amount: 500 },
+    { id: 2, type: "expense", amount: 200 },
+    { id: 3, type: "income", amount: 300 },
+    { id: 4, type: "expense", amount: 150 },
+  ]);
+
   return <div className="App"></div>;
 }
 
