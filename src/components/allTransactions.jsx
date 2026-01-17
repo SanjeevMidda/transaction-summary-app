1753e491abaf724c9f6744ebc6d1865449b2f580
Sanjeev Midda <s.midda1@gmail.com>
2026-01-17 21:25:18 +0000

    created components folder to hold three components: all transactions, eexpense type, and incomeType

---
+++ src/components/allTransactions.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const allTransactions = () => {
+  return <div>allTransactions</div>;
+};
+
+export default allTransactions;
---
+++ src/components/incomeType.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const incomeType = () => {
+  return <div>incomeType</div>;
+};
+
+export default incomeType;
