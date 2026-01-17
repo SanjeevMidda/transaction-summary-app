0fac433bae76caff32555fd9a34398855212d00e
Sanjeev Midda <s.midda1@gmail.com>
2026-01-17 21:33:29 +0000

    removed data from useState and created a separate file, named transactionData, to hold all transactions instead

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import "./index.css";
 import { useState } from "react";
 
 function App() {
-  const [transactions, setTransactions] = useState([
-    { id: 1, type: "income", amount: 500 },
-    { id: 2, type: "expense", amount: 200 },
-    { id: 3, type: "income", amount: 300 },
-    { id: 4, type: "expense", amount: 150 },
-  ]);
+  const [transactions, setTransactions] = useState();
 
   const totalIncome = () => {};
   const totalExpenses = () => {};
---
+++ src/data/transactionData.js
@@ -0,0 +1,6 @@
+export const transactions = [
+  { id: 1, type: "income", amount: 500 },
+  { id: 2, type: "expense", amount: 200 },
+  { id: 3, type: "income", amount: 300 },
+  { id: 4, type: "expense", amount: 150 },
+];
