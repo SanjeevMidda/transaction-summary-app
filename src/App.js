79a913094660738d0eb3fdc0775bef2e0dcd960c
Sanjeev Midda <s.midda1@gmail.com>
2026-01-17 22:53:35 +0000

    adding ui and styles for this for our App.js component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,34 @@ function App() {
   const totalExpenses = () => {};
   const finalBalance = () => {};
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <id className="transactionsContainerMain">
+        <h1>
+          TRANSACTION <br />
+          SUMMARY
+        </h1>
+
+        <div id="allTransactionsContainer">
+          {transactionData.map((item) => {
+            return (
+              <div>
+                <h4>Type {item.type}</h4>
+                <h4>Amount {item.amount}</h4>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="filters"></div>
+        <div className="calculations">
+          <h3>Total Income</h3>
+          <h3>Total Expenses</h3>
+          <h3>Final Balance</h3>
+        </div>
+      </id>
+    </div>
+  );
 }
 
 export default App;
